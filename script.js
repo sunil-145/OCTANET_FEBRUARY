c74d0e74dd6ff3f19c7b8bb58821d097a56be5b7
sunil-145 <146164003+sunil-145@users.noreply.github.com>
2024-02-28 15:13:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+
+  if (taskInput.value.trim() !== "") {
+    var taskItem = document.createElement("li");
+    taskItem.textContent = taskInput.value;
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  } else {
+    alert("Please enter a task!");
+  }
+}
